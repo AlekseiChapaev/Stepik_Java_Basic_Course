c67034bc0323dbe459a02fa1d991b933f58acea4
Aleksei Chapaev <alekseiachapaev@gmail.com>
2023-02-03 17:19:23 +0100

    add IsPalindrome

---
+++ src/IsPalindrome.java
@@ -0,0 +1,16 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class IsPalindrome {
+    public static boolean isPalindrome(String text) {
+        String res = text.toLowerCase().replaceAll("[^a-zA-Z1-9]", "");
+        StringBuilder sb = new StringBuilder(res);
+
+        return res.equals(sb.reverse().toString());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("Madam, I'm Adam!"));
+        System.out.println();
+    }
+}
